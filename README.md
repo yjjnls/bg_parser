ef67eb309acfd2ef8522dc0a7b28859a6db9ca02
yjjnls <x-jj@foxmail.com>
2018-11-22 20:04:37 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bg_parser
 Statistical plugin for blockgeek
+
+## How to start
+1.  install [node.js](https://nodejs.org/dist/v10.13.0/node-v10.13.0-x64.msi)
+2.  git clone https://github.com/yjjnls/bg_parser.git
+3.  cd bg_parser
+4.  npm install
+
+5.  copy member list to `member.txt`
+6.  npm start
+7.  parsing result will stroe in dir `result`
---
+++ parser.js
@@ -0,0 +1,140 @@
+const puppeteer = require('puppeteer');
+var fs = require('fs');
+var xlsx = require('node-xlsx');
+
+function sleep(numberMillis) {
+    var now = new Date();
+    var exitTime = now.getTime() + numberMillis;
+    while (true) {
+        now = new Date();
+        if (now.getTime() > exitTime)
+            return;
+    }
+}
+async function parse_topic(page, arr) {
+    topic = [];
+    index = -1;
+    for (var i = 0; i < arr.length; ++i) {
+        let val = arr[i];
+        if (val.indexOf("link-top-line") > 0) {
+            index++;
+            res = val.match('href="(.*)" class="title raw-link raw-topic-link">(.*)</a>');
+            if (res) {
+                topic[index] = { 'url': 'http://blockgeek.org' + res[1], 'name': res[2] };
+            }
+            await page.goto(topic[index].url);
+            // await page.waitFor(1000);
+            let content = await page.$eval('#post_1 > div > div.topic-body.clearfix > div.regular.contents > div', el => el.innerText);
+            if (content.length >= 500) {
+                topic[index].type = 'article';
+            } else {
+                topic[index].type = 'question';
+            }
+        }
+        if (val.indexOf("category-name") > 0) {
+            res = val.match('class="category-name">(.*)</span></span></a></td>');
+            if (res) {
+                topic[index].category = res[1];
+            }
+        }
+        if (val.indexOf("最早帖子") > 0) {
+            res = val.match('最早帖子: (.*)');
+            if (res) {
+                topic[index].time = res[1];
+            }
+        }
+
+    }
+    return topic;
+}
+
+function parse_reply(arr) {
+    topic = [];
+    index = -1;
+    // console.log(arr);
+    arr.forEach(val => {
+        if (val.indexOf('relative-date date') > 0) {
+            index++;
+            res = val.match('class="relative-date date" title="(.*)" data-time=');
+            if (res) {
+                topic[index] = { 'time': res[1], 'type': 'answer' };
+            }
+        }
+        if (val.indexOf("category-name") > 0) {
+            res = val.match('class="category-name">(.*)</span></span></a></div>');
+            if (res) {
+                topic[index].category = res[1];
+            }
+        }
+        if (val.indexOf('href="/t/topic') > 0) {
+            res = val.match('href="(.*)">(.*)</a>');
+            if (res) {
+                topic[index].url = 'http://blockgeek.org' + res[1];
+                topic[index].name = res[2];
+            }
+        }
+    });
+    return topic;
+}
+
+async function scroll(page, scrollDelay = 1000) {
+    for (var i = 0; i < 5; ++i) {
+        previousHeight = await page.evaluate('document.body.scrollHeight');
+        await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+        await page.waitFor(500);
+    }
+}
+function writeResult(name, data) {
+    let list = [['topic', 'url', 'time', 'category', 'type']];
+    data.forEach(val => {
+        let arr = [];
+        arr.push(val.name);
+        arr.push(val.url);
+        arr.push(val.time);
+        arr.push(val.category);
+        arr.push(val.type);
+        list.push(arr);
+    });
+    var buffer = xlsx.build([
+        {
+            name: name,
+            data: list
+        }
+    ]);
+    fs.writeFileSync('result/' + name + '.xlsx', buffer, { 'flag': 'w' });
+}
+async function parse(username) {
+    console.log('===>parsing ' + username);
+    id = username.toLowerCase();
+    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+    const page = await browser.newPage();
+    await page.goto(`http://blockgeek.org/u/${id}/activity/topics`);
+    // await page.waitFor(1000);
+    await scroll(page);
+    let content = await page.$eval('#main-outlet > div:nth-child(2)', el => el.innerHTML);
+    let arr = content.split('\n');
+
+    let topic = await parse_topic(page, arr);
+
+    await page.goto(`http://blockgeek.org/u/${id}/activity/replies`);
+    await page.waitFor(1000);
+    await scroll(page);
+    content = await page.$eval('#main-outlet > div:nth-child(2)', el => el.innerHTML);
+    arr = content.split('\n');
+    let answer = parse_reply(arr);
+    // console.log(answer);
+    // console.log(topic.concat(answer));
+    await browser.close();
+    writeResult(username, topic.concat(answer));
+    console.log(`===>parsing ${username} done!`);
+
+}
+
+var data = fs.readFileSync('./member.txt', 'utf8').split('\n');
+
+(async () => {
+    for (var i = 0; i < data.length; ++i) {
+        await parse(data[i]);
+    }
+})();
